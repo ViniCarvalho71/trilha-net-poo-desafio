bf36cacf90949ce85df1a18ac9526d4fc7da41c7
ViniCarvalho71 <carvalhovinicius71@gmail.com>
2024-08-25 17:12:24 -0300

    finalizando o desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,10 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone proMax = new Iphone("14 996164842","15 ProMax", "e2w3q4234324", 8);
+Nokia nokia = new Nokia("14 996164842", "nokia", "ee32432e2343254", 2);
+
+proMax.InstalarAplicativo("Minecraft");
+nokia.InstalarAplicativo("Jogo da cobrinha");
+
+proMax.Ligar();
+nokia.ReceberLigacao();
